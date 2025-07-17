53f74e15b3bfa7aa43c55d5d97f2eedbabdc612a
BlackOwl242 <swsheetjs@gmail.com>
2025-07-18 02:34:57 +0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Frame } from "./screens/Frame";
-import "../tailwind.css";
+import "./index.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
